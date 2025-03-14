9d50ab6f9d1f3e2218413981ec31c62e51afd92e
nathan74490 <na.boudet@formation-cci.fr>
2025-03-14 15:54:10 +0100

    modif index

--- index.php
+++ index.php
@@ -9,15 +9,24 @@
 
     <div id="header">
         <h1>Header de la page</h1>
+        <?php
+            include 'header.php';
+        ?>
     </div>
 
     <div id="content">
         <h2>Contenu principal</h2>
         <p>Ceci est la zone de contenu où vous pouvez ajouter du texte, des images, etc.</p>
+        <?php
+            include 'content.php';
+        ?>
     </div>
 
     <div id="footer">
         <p>Pied de page © 2025</p>
+        <?php
+            include 'footer.php';
+        ?>
     </div>
 
 </body>
