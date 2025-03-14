ce433ed179bdb27bde37e2cea2e868da3a0ccf96
nathan74490 <na.boudet@formation-cci.fr>
2025-03-14 16:27:31 +0100

    modif content index

--- index.php
+++ index.php
@@ -1,33 +1,49 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page de base</title>
 </head>
+
 <body>
 
     <div id="header">
-        <h1>Header de la page</h1>
+        <!-- <h1>Header de la page</h1> -->
         <?php
-            include 'header.php';
+        include 'header.php';
         ?>
     </div>
 
     <div id="content">
-        <h2>Contenu principal</h2>
-        <p>Ceci est la zone de contenu où vous pouvez ajouter du texte, des images, etc.</p>
+        <!-- <h2>Contenu principal</h2> -->
+        
         <?php
-            include 'content.php';
+            // print_r($_GET);
+            $page = isset($_GET['pages']) ? $_GET['pages'] : 'content';
+            $chemin =  $page . '.php';
+            if (file_exists($chemin)) {
+                include $chemin;
+            } else {
+                echo 'erreur 404';
+            }
+            // include 'content.php';
         ?>
     </div>
 
     <div id="footer">
-        <p>Pied de page © 2025</p>
+        <!-- <p>Pied de page © 2025</p> -->
         <?php
+            
+
             include 'footer.php';
         ?>
     </div>
 
 </body>
+
 </html>
+<?php
+
+?>
